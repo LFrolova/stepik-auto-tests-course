f6234d5b0841aee542ad9cb47ddd5b821043828b
LFrolova <luka@nightmail.ru>
2021-09-06 12:20:02 +0300

    добавление урока 2_lesson4_step8

---
+++ 2_lesson4_step8.py
@@ -0,0 +1,42 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import math
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+
+browser.get("http://suninjuly.github.io/explicit_wait2.html")
+
+# говорим Selenium ждать в течение 12 секунд, пока цена не станет 100
+button = WebDriverWait(browser, 12).until(EC.text_to_be_present_in_element((By.ID, "price"), "$100"))
+button1 = browser.find_element_by_id("book")
+button1.click()
+
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+
+try:    
+    x_element = browser.find_element_by_id("input_value")
+    x = x_element.text
+    # Подставляем считанное значение в функцию
+    y = calc(x)
+    # Вписать полученное значение в поле
+    input1 = browser.find_element_by_id("answer")
+    input1.send_keys(y)
+
+
+    # Отправляем заполненную форму
+    button = browser.find_element_by_id("solve")
+    button.click()
+
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+
